e798b088bd21f4942965c2b4bb92058e388379ac
Markus Ketterer <markus.ketterer@gugler.at>
2023-12-06 12:33:46 +0100

    add option to prepend date to filename

--- Classes/Domain/Model/EZLogger.php
+++ Classes/Domain/Model/EZLogger.php
@@ -6,47 +6,52 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 
-class EZLogger {
+class EZLogger
+{
+  private $fileWriter;
+  private $varPath;
+  private $timestamp;
+  private $extensionConfiguration;
 
-    private $fileWriter;
-    private $varPath;
-    private $timestamp;
-    private $extensionConfiguration;
+  function __construct(string $filename = "ez-logging.log", bool $prependDateTime = false)
+  {
+    //Get config from ext_conf_template.txt
+    $this->extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get("t3ezlogger");
+    //Create File
+    $this->varPath = Environment::getVarPath() . "/log/";
 
+    $this->openFileWriter($filename, $prependDateTime);
+  }
 
-
-    function __construct(string $filename = "ez-logging.log") {
-      //Get config from ext_conf_template.txt
-      $this->extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('t3ezlogger');
-      //Create File
-      $this->varPath = Environment::getVarPath()."/log/";
-      $this->openFileWriter($this->varPath.$filename);
-    }
-
-    public function writeLog ($msg){
-      if($this->extensionConfiguration["activateLog"] == '1'){
-        try {
-            $this->timestamp = date('Y-m-d H:i:s');
-            if (is_string($msg)) {
-              fwrite($this->fileWriter,$this->timestamp.": ". $msg);
-          } else if (is_array($msg)){
-            fwrite($this->fileWriter,$this->timestamp.": ". print_r($msg,true));
-          }
-          else{
-            throw new Exception('Invalid variable type for $msg');
-          }
-        } catch (\Throwable $th) {
-            throw $th;
-            $this->closeFileWriter();
+  public function writeLog($msg)
+  {
+    if ($this->extensionConfiguration["activateLog"] == "1") {
+      try {
+        $this->timestamp = date("Y-m-d H:i:s");
+        if (is_string($msg)) {
+          fwrite($this->fileWriter, $this->timestamp . ": " . $msg);
+        } elseif (is_array($msg)) {
+          fwrite($this->fileWriter, $this->timestamp . ": " . print_r($msg, true));
+        } else {
+          throw new Exception('Invalid variable type for $msg');
         }
+      } catch (\Throwable $th) {
+        throw $th;
+        $this->closeFileWriter();
       }
     }
+  }
 
-    public function openFileWriter ($filename){
-        $this->fileWriter = fopen($filename, "a");
+  public function openFileWriter(string $filename, bool $prependDateTime)
+  {
+    if ($prependDateTime) {
+      $filename = date("Ymd_") . $filename;
     }
+    $this->fileWriter = fopen($this->varPath . $filename, "a");
+  }
 
-    public function closeFileWriter (){
-        fclose($this->fileWriter);
-    }
+  public function closeFileWriter()
+  {
+    fclose($this->fileWriter);
+  }
 }
--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Sometimes you just need a little workaround to debug your code
 composer req bokuno/t3-ez-logger
 
 ## How to use this logger
-The logger needs to be activated to write into the log file. So you can easily de-/activate this feature without installing and removing the extension. 
+The logger needs to be activated to write into the log file. So you can easily de-/activate this feature without installing and removing the extension.
 
-Then you can use this code to log: 
+Then you can use this code to log:
 ```
 use BokuNo\T3EZLogger\Domain\Model\EZLogger;
 ...
@@ -17,11 +17,19 @@ $ezlogger->write("I want to debug this");
 ```
 check for log in var/log/filename.log
 
+## Options
+### prepend current date to filename
+just add "true" to constructor to prepend the current date in format "Ymd" to the filename
+```
+$ezlogger = new EZLogger("filename.log",true);
+```
+
+
 ## But why ? I can log directly via \TYPO3\CMS\Core\Log\LogManager
 
-Because when I need to debug strange things in production, I don't want to flood the server with unnecessery logs from extensions I cannot fix for reasons. 
+Because when I need to debug strange things in production, I don't want to flood the server with unnecessery logs from extensions I cannot fix for reasons.
 
 ##TODO:
 - Scheduler to remove log after n days
 - Send Mail Function to directly send your log via mail to predefined mail address
-- Timestamp to prepend to log file? 
+- Timestamp to prepend to log file?
