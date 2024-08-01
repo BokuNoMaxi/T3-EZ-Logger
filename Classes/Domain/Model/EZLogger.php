3573c171eecc36a0da94fca48b1377bd75fd3403
Markus Ketterer <markus.ketterer@gugler.at>
2024-08-01 12:21:00 +0200

    add sendMail functionality

--- Classes/Domain/Model/EZLogger.php
+++ Classes/Domain/Model/EZLogger.php
@@ -2,20 +2,26 @@
 
 namespace BokuNo\T3EZLogger\Domain\Model;
 
+use Symfony\Component\Mime\Address;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Mail\MailMessage;
+
 
 class EZLogger
 {
   private $fileWriter;
-  private $varPath;
-  private $timestamp;
+  private string $varPath;
+  private string $timestamp;
   private $extensionConfiguration;
 
-  function __construct(string $filename = "ez-logging.log", bool $prependDateTime = false)
-  {
+  public function __construct(
+    protected string $filename = "ez-logging.log",
+    protected bool $prependDateTime = false,
+    protected bool $attachToLogfile = false
+  ) {
     //Get config from ext_conf_template.txt
     $this->extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get("t3ezlogger");
     //Create File
@@ -27,7 +33,6 @@ class EZLogger
   public function writeLog(array|string $msg, bool $newLine = TRUE)
   {
     if ($this->extensionConfiguration["activateLog"] == "1") {
-      try {
         $this->timestamp = date("Y-m-d H:i:s");
         if (is_string($msg)) {
           fwrite($this->fileWriter, $this->timestamp . ": " . $msg);
@@ -38,17 +43,19 @@ class EZLogger
         } else {
           throw new Exception('Invalid variable type for $msg');
         }
-      } catch (\Throwable $th) {
-        throw $th;
-        $this->closeFileWriter();
-      }
     }
   }
 
   private function addNewLine(bool $newLine)
   {
-    if($newLine)
-      fwrite($this->fileWriter, "\n");
+    if ($newLine) {
+        fwrite($this->fileWriter, "\n");
+    }
+  }
+
+  private function getLogFile()
+  {
+    return $this->varPath . $this->filename;
   }
 
   public function openFileWriter(string $filename, bool $prependDateTime)
@@ -56,11 +63,45 @@ class EZLogger
     if ($prependDateTime) {
       $filename = date("Ymd_") . $filename;
     }
-    $this->fileWriter = fopen($this->varPath . $filename, "a");
+    $mode = "w";
+    if ($this->attachToLogfile) {
+      $mode = "a";
+    }
+    $this->fileWriter = fopen($this->varPath . $filename, $mode);
   }
 
   public function closeFileWriter()
   {
     fclose($this->fileWriter);
+  }
+
+  /**
+   * if a receiver email (normally developer E-Mail to watch for critical tasks) is set in the extension configuration it will send the log file
+   * you can set a subject and a default sender address,
+   * as fallback for the sender address the one from the extension configuration will be used and the fallback from the configuration is the typo3 default sender address
+   */
+  public function sendMail(string $subject = "", Address|string $to = "", Address|string $sender = ""): bool
+  {
+    if (
+      $this->extensionConfiguration["activateLog"] == "1" &&
+      ($to || $this->extensionConfiguration["mailReceiver"])
+    ) {
+      $to = $to ?: $this->extensionConfiguration["mailReceiver"];
+      $mail = GeneralUtility::makeInstance(MailMessage::class);
+      $mail
+        ->setSubject($subject ?? 'Report from EZ Logger')
+        ->setTo($to)
+        ->text("Hello, \n\nA new logfile has been generated. You can find it attached to this mail. \n\nHave a nice day!")
+        ->attachFromPath($this->getLogFile(), 'logfile.log');
+      if ($sender || $this->extensionConfiguration["mailSender"]) {
+        $sender = $sender ?: $this->extensionConfiguration["mailSender"];
+        $mail
+          ->from($sender)
+          ->sender($sender);
+      }
+
+      return $mail->send();
+    }
+    return false;
   }
 }
--- README.md
+++ README.md
@@ -25,16 +25,36 @@ just add "true" to constructor to prepend the current date in format "Ymd" to th
 $ezlogger = new EZLogger("filename.log",true);
 ```
 
-### toggle logging
-sometimes you don't want to flood your server with logs, or you log to much and have performance problems, so I made an option to easily toggle if ezlogger writes into the log or not:
-![Toggle Log](Documentation/Images/ToggleLog.png)
+### send mail
+To send the log per mail you can easily call this function:
+```
+$ezlogger = new EZLogger("filename.log");
+....
+$ezlogger->sendMail($subject,$to,$sender);
+...
+```
+None of these parameters are required, if they are set in the extension configuration. Further more mails will only be send if the logging is activated (else the log file is empty...)
+
+
+## Extension configuration
+![Extension configuration](Documentation/Images/ExtensionConfiguration.png)
+### activateLog
+sometimes you don't want to flood your server with logs, or you log to much and have performance problems, so I made an option to easily toggle if ezlogger writes into the log or not.
+
+### mailReceiver
+if activateLog = 1, the system can send out the logfile per mail to a predefined receiver. You can configure the receiver globally in the extension or in the function call
+
+### mailSender
+if activateLog = 1, the system can send out the logfile per mail to a predefined receiver. You can configure the sender globally in the extension or in the function call
+
 
 
 ## But why ? I can log directly via \TYPO3\CMS\Core\Log\LogManager
 
 Because when I need to debug strange things in production, I don't want to flood the server with unnecessery logs from extensions I cannot fix for reasons.
 
-##TODO:
+## TODO:
+just some features that are nice to have, that you don't want to implement every single time...
 - Scheduler to remove log after n days
-- Send Mail Function to directly send your log via mail to predefined mail address
+- ~~Send Mail Function to directly send your log via mail to predefined mail address~~
 - ~~Timestamp to prepend to log file?~~
